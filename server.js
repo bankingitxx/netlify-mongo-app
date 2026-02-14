ebf92f58ca66be8a24be45f4f59d2a113ef7a5da
Bank It’S Me BaNk <itsmebank@MacBook-Air-2.local>
2026-02-14 21:26:05 +0700

    Change API port from 3001 to 5050 to match firewall settings

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5050;
 
 // Middleware
 app.use(cors());
